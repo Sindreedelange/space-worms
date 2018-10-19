bf1d7e524ba41583b6943a5455e42ddfe873b399
Sindreedelange <146169@stud.hvl.no>
2018-10-19 17:30:53 +0200

    Board object initialized

--- class/board.py
+++ class/board.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 
 class Board(object):
@@ -6,10 +7,10 @@ class Board(object):
     def __init__(self, initialize=True):
         self.base_url = "https://visningsrom.stacc.com/dd_server_worms/rest/boards/2"
         if initialize:
-            self.initialize-board()
+            self.initialize_board()
 
     def initialize_board(self):
-        response = request.get(self.base_url)
+        response = requests.get(self.base_url)
         data = response.json
         print("Response status code", data.status_code)
         print("Data: \n", data.content)
