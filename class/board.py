174dfd9f6b0382fa9b455a10038db0616cfa4a53
Sindreedelange <146169@stud.hvl.no>
2018-10-19 18:11:03 +0200

    Board initialized from API Get request

--- class/board.py
+++ class/board.py
@@ -5,15 +5,38 @@ import requests
 class Board(object):
 
     def __init__(self, initialize=True):
-        self.base_url = "https://visningsrom.stacc.com/dd_server_worms/rest/boards/2"
+        base_url = "https://visningsrom.stacc.com/dd_server_worms/rest/boards/2"
         if initialize:
-            self.initialize_board()
+            data = self.initialize_board(base_url).json()
+            self.id = data['id']
+            self.name = data['name']
+            self.description = data['description']
+            self.size = data['size']
+            self.dimX = int(data['dimX'])
+            self.dimY = int(data['dimY'])
+            self.start = int(data['start'])
+            self.goal = int(data['goal'])
+            self.api_ref = data['self']
+            self.start_api_ref = data['startSquare']
+        print(self.dimX)
 
-    def initialize_board(self):
-        response = requests.get(self.base_url)
-        data = response.json
-        print("Response status code", data.status_code)
-        print("Data: \n", data.content)
+    def initialize_board(self, base_url):
+        """ Initialize the board
+
+        Initialize the board using URL for appropriate API
+
+        Args:
+            Explain the arguments
+
+        Returns:
+            Return type
+        """
+        response = requests.get(base_url)
+        if response.status_code != 200:
+            print("Something wrong with API call \n Try again?")
+        else:
+            return response
+
 
     def function(self, param):
         """ Quick description
@@ -26,3 +49,5 @@ class Board(object):
         Returns:
             Return type 
         """
+
+board = Board()
