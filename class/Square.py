277854666648c951c35fb73ec59f96030cfa91c7
Sindreedelange <146169@stud.hvl.no>
2018-10-19 23:08:39 +0200

    Add more features

--- class/Square.py
+++ class/Square.py
@@ -1,8 +1,11 @@
 class Square(object):
 
-    def __init__(self, number, pos_x, pos_y, name, links):
+    def __init__(self, number, pos_x, pos_y, name, links, wormhole=0, wormhole_url=""):
         self.number = number
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.name = name
         self.links = links
+        self.wormhole = wormhole
+        self.wormhole_url = wormhole_url
+
