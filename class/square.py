b1658d89493b4dc4b627ef31723d35a9c0380398
Sindreedelange <146169@stud.hvl.no>
2018-10-19 21:51:45 +0200

    Initialized square object

---
+++ class/square.py
@@ -0,0 +1,8 @@
+class Square(object):
+
+    def __init__(self, number, pos_x, pos_y, name, links):
+        self.number = number
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.name = name
+        self.links = links
