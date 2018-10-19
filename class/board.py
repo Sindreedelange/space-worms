a47a28143cc5d825ce952be3934de60eb6421ea1
Sindreedelange <146169@stud.hvl.no>
2018-10-19 21:51:59 +0200

    Added docstring

--- class/board.py
+++ class/board.py
@@ -5,38 +5,35 @@ import requests
 class Board(object):
 
     def __init__(self, initialize=True):
-        base_url = "https://visningsrom.stacc.com/dd_server_worms/rest/boards/2"
+        self.base_url = "https://visningsrom.stacc.com/dd_server_worms/rest/boards/2"
         if initialize:
-            data = self.initialize_board(base_url).json()
+            data = self.initialize_board.json()
             self.id = data['id']
             self.name = data['name']
             self.description = data['description']
             self.size = data['size']
             self.dimX = int(data['dimX'])
             self.dimY = int(data['dimY'])
-            self.start = int(data['start'])
-            self.goal = int(data['goal'])
+            self.start_square = int(data['start'])
+            self.goal_square = int(data['goal'])
             self.api_ref = data['self']
-            self.start_api_ref = data['startSquare']
+            self.start__square_api_ref = data['startSquare']
         print(self.dimX)
 
-    def initialize_board(self, base_url):
-        """ Initialize the board
-
-        Initialize the board using URL for appropriate API
-
-        Args:
-            Explain the arguments
-
-        Returns:
-            Return type
+    @property
+    def initialize_board(self):
         """
-        response = requests.get(base_url)
+        Initialize the board
+
+        Initialize the board using pre-defined URL
+
+        :return: requests.models.response
+        """
+        response = requests.get(self.base_url)
         if response.status_code != 200:
             print("Something wrong with API call \n Try again?")
         else:
             return response
-
 
     def function(self, param):
         """ Quick description
@@ -49,5 +46,6 @@ class Board(object):
         Returns:
             Return type 
         """
+
 
 board = Board()
