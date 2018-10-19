91dd06d0c4aa7b4e57d10c6cb3aae88a5bae26f4
Sindreedelange <146169@stud.hvl.no>
2018-10-20 00:48:09 +0200

    Printing the entire board when finising populating it

--- class/SpaceWorms.py
+++ class/SpaceWorms.py
@@ -4,7 +4,8 @@ from Board import Board
 def main():
     board = Board()
     board.populate_board()
-    print(board.squares)
+    for square in board.squares:
+        print("Main method: ", square.number)
 
 main()
 
