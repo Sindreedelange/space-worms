93bd7e3ebbc64172fba2ee409012add73d2f4d81
Sindreedelange <146169@stud.hvl.no>
2018-10-20 14:43:41 +0200

    Print = debug

--- class/SpaceWorms.py
+++ class/SpaceWorms.py
@@ -4,6 +4,7 @@ from Board import Board
 def main():
     board = Board()
     board.populate_board()
+    print("Populate board done")
     for square in board.squares:
         print("Main method: ", square.number)
 
