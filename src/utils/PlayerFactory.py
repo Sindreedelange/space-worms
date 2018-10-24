fc540c37fe3beb868db4d799f69dea70c4c16282
Sindreedelange <146169@stud.hvl.no>
2018-10-24 10:08:21 +0200

    PlayerFactory init

---
+++ src/utils/PlayerFactory.py
@@ -0,0 +1,12 @@
+from src.model.Player import Player
+
+
+class PlayerFactory:
+
+    @staticmethod
+    def initialize_players(n_players, start_square):
+        players = []
+        for i in range(n_players):
+            new_player = Player(i, start_square)
+            players.append(new_player)
+        return players
